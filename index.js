206f5e38ba6883a4a09ecf18da90688bc313957f
Brian Woodward <brian.woodward@gmail.com>
2014-12-09 22:12:55 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * gulp-routes <https://github.com/assemble/gulp-routes>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
