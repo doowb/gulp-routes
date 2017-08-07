ea919b1dc1cbc052e188fc8b876d4c0393372352
jonschlinkert <jon.schlinkert@sellside.com>
2017-08-07 03:12:25 -0400

    lint, use assert

--- test.js
+++ test.js
@@ -1,30 +1,29 @@
 /*!
  * gulp-routes <https://github.com/assemble/gulp-routes>
  *
- * Copyright (c) 2014-2015, Brian Woodward.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2017, Brian Woodward.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-var should = require('should');
+var File = require('vinyl');
+var assert = require('assert');
 var through = require('through2');
-var File = require('gulp-util').File;
 var Router = require('en-route').Router;
 var gulpRoutes = require('./');
 
 describe('gulp-routes', function() {
-  it('should route files', function(done) {
-
+  it('should route files', function(cb) {
     var fakeFile = new File({
       cwd: 'fixtures',
       base: 'fixtures',
       path: 'fixtures/index.js',
-      contents: new Buffer('var foo = function(){};')
+      contents: new Buffer('var foo = function() {};')
     });
 
     var router = new Router();
-    router.all(/\.js/, function (file, next) {
+    router.all(/\.js/, function(file, next) {
       file.data = file.data || {};
       file.data.middleware = true;
       next();
@@ -32,27 +31,26 @@ describe('gulp-routes', function() {
 
     var routes = gulpRoutes(router);
     var stream = routes();
-    stream.on('data', function (file) {
-      file.data.middleware.should.be.true;
+    stream.on('data', function(file) {
+      assert(file.data.middleware);
     });
 
-    stream.on('end', done);
-
+    stream.on('end', cb);
     stream.write(fakeFile);
     stream.end();
   });
 
-  it('should route files with a router from `this`', function (done) {
+  it('should route files with a router from `this`', function(cb) {
     var fakeFile = new File({
       cwd: 'fixtures',
       base: 'fixtures',
       path: 'fixtures/index.js',
-      contents: new Buffer('var foo = function(){};')
+      contents: new Buffer('var foo = function() {};')
     });
 
     var app = {};
     app.router = new Router();
-    app.router.all(/\.js/, function (file, next) {
+    app.router.all(/\.js/, function(file, next) {
       file.data = file.data || {};
       file.data.middleware = true;
       next();
@@ -60,17 +58,16 @@ describe('gulp-routes', function() {
 
     var routes = gulpRoutes.call(app);
     var stream = routes();
-    stream.on('data', function (file) {
-      file.data.middleware.should.be.true;
+    stream.on('data', function(file) {
+      assert(file.data.middleware);
     });
 
-    stream.on('end', done);
-
+    stream.on('end', cb);
     stream.write(fakeFile);
     stream.end();
   });
 
-  it('should route files to different routes', function (done) {
+  it('should route files to different routes', function(cb) {
     var file1 = new File({
       cwd: 'fixtures',
       base: 'fixtures',
@@ -86,44 +83,45 @@ describe('gulp-routes', function() {
     });
 
     var router = new Router();
-    router.use(function (file, next ) {
+    router.use(function(file, next) {
       file.data = file.data || {};
       next();
     });
 
-    router.all(/one/, function (file, next) {
+    router.all(/one/, function(file, next) {
       file.data.one = true;
       next();
     });
 
-    router.all(/two/, function (file, next) {
+    router.all(/two/, function(file, next) {
       file.data.two = true;
       next();
     });
 
     var routes = gulpRoutes(router);
     var stream = routes();
-    stream.on('data', function (file) {
+
+    stream.on('data', function(file) {
       switch (file.contents.toString()) {
         case 'one':
-          file.data.one.should.be.true;
-          file.data.should.not.have.property('two');
+          assert(file.data.one);
+          assert(!file.data.two);
           break;
         case 'two':
-          file.data.should.not.have.property('one');
-          file.data.two.should.be.true;
+          assert(!file.data.one);
+          assert(file.data.two);
           break;
       }
     });
 
-    stream.on('end', done);
-
+    stream.on('error', cb);
+    stream.on('end', cb);
     stream.write(file1);
     stream.write(file2);
     stream.end();
   });
 
-  it('should route on multiple methods', function (done) {
+  it('should route on multiple methods', function(cb) {
     var fakeFile = new File({
       cwd: 'fixtures',
       base: 'fixtures',
@@ -135,22 +133,22 @@ describe('gulp-routes', function() {
       methods: ['before', 'after', 'whatever']
     });
 
-    router.use(function (file, next) {
+    router.use(function(file, next) {
       file.data = file.data || {};
       next();
     });
 
-    router.before(/./, function (file, next) {
+    router.before(/./, function(file, next) {
       file.data.before = true;
       next();
     });
 
-    router.whatever(/./, function (file, next) {
+    router.whatever(/./, function(file, next) {
       file.data.whatever = true;
       next();
     });
 
-    router.after(/./, function (file, next) {
+    router.after(/./, function(file, next) {
       file.data.after = true;
       next();
     });
@@ -161,39 +159,38 @@ describe('gulp-routes', function() {
     var afterStream = routes('after');
 
     whateverStream
-      .pipe(through.obj(function (file, enc, cb) {
-        file.data.whatever.should.be.true;
-        file.data.should.not.have.property('after');
+      .pipe(through.obj(function(file, enc, cb) {
+        assert(file.data.whatever);
+        assert(!file.data.after);
         this.push(file);
         cb();
-      }))
+      }));
 
     beforeStream
-      .pipe(through.obj(function (file, enc, cb) {
-        file.data.before.should.be.true;
-        file.data.should.not.have.property('after');
+      .pipe(through.obj(function(file, enc, cb) {
+        assert(file.data.before);
+        assert(!file.data.after);
         this.push(file);
         cb();
       }))
       .pipe(whateverStream)
       .pipe(afterStream);
 
-    afterStream.on('data', function (file) {
-      file.data.before.should.be.true;
-      file.data.whatever.should.be.true;
-      file.data.after.should.be.true;
+    afterStream.on('data', function(file) {
+      assert(file.data.before);
+      assert(file.data.whatever);
+      assert(file.data.after);
     });
 
-    afterStream.on('end', done);
+    afterStream.on('end', cb);
 
     beforeStream.write(fakeFile);
     beforeStream.end();
   });
 
-  it('should throw an error when a router is\'t provided.', function (done) {
-    (function () {
-      gulpRoutes()
-    }).should.throw('Expected a valid router object.');
-    done();
+  it('should throw an error when a router is\'t provided.', function() {
+    assert.throws(function() {
+      gulpRoutes();
+    });
   });
 });
